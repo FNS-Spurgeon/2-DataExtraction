defa6555ed119caa9eb3831b0fdb8f0d919016c3
leblance <elina.leblanc@live.fr>
2026-02-05 16:43:46 +0100

    Update Regex and improve script

--- scripts/allusionsplit.py
+++ scripts/allusionsplit.py
@@ -1,24 +1,29 @@
 import re
 import os
 
-txtFolder = "/content"
+txtFolder = "../testFiles_split"
 
 for txt in os.listdir(txtFolder):
   txt_path = os.path.join(txtFolder, txt)
   label, ext = os.path.splitext(txt_path)
+
   if txt.endswith('.txt'):
     with open(txt_path, "r") as f:
+      # We get the first line of each line to preserve page numbers
       content = f.readline()
-      # print(content)
-
       textFile = f.read()
 
-      pattern = r'(^[0-9]{4}(\-[0-9]{1,4})*\.\s[A-Z]*\[*|^\[*[0-9]{4}(\-*\/*[0-9]{1,4})*\.*\?*\]*\s[A-Z]\[*|^\[[ac\. ]*[0-9]{4}(\-*\/*[0-9]{1,4})*\.*\?*\]*\s[A-Z]\[*)'
-      # pattern = r'(^"*\s*\[*([0-9]{4})*\]*\.*,*\]* *([A-Z]*[July|Oct.]{3})* \d*)'
+      # Regular expression to find different format of dates
+      pattern = r'((^\[?\d{4}-*\/*\d{0,4}[\.\?]?\]?|^\[?([nac]\.)+\s\d{4}[\.?]?\]?|^\[\d{2}\]\d{2}\.)(\s[?[A-Z]))'
+
+      # We add two lines before entry dates
       splitText = re.sub(pattern, r"\n\n\1", textFile, flags=re.M)
+
+      # We split the text by dates
       newText = re.split(r"\n?\n\n", splitText)
       # print(newText)
 
+      # Each entry is exported in a new .txt file
       allusionNb = 1
       for allusion in newText[1:]:
         name = label + "_allusion_" + str(allusionNb) + ".txt"
--- scripts/dataextraction.py
+++ scripts/dataextraction.py
@@ -2,10 +2,11 @@ import re
 import os
 import csv
 
-txtFolder = "../testFiles"
+txtFolder = "../testFiles_extraction"
 
-dateRegEx = r'(^[0-9]{4}(\-[0-9]{1,4})*\.\s[A-Z]*\[*|^\[*[0-9]{4}(\-*\/*[0-9]{1,4})*\.*\?*\]*\s[A-Z]\[*|^\[[ac\. ]*[0-9]{4}(\-*\/*[0-9]{1,4})*\.*\?*\]*\s[A-Z]\[*)'
-authorRegEx = r"\d\.*[\.\]]\s(Unknown\.|[A-Z]\.,*\s*[A-Z]*\.*|\[*(D')*(De\s)*([A-Z][a-z]+-)*([A-Z][a-z]+)(,\s)\[*([A-Z][a-z]+\s)*([A-Z][a-z]+)(,\s)*(of|de)*\s*([A-Z][a-z]+)*(\sof|de)*(\s[A-Z][a-z]+)*\(*\?*\)*\.*\]*)"
+dateRegEx = r'\n(^\[?\d{4}-*\/*\d{0,4}[\.\?]?\]?|^\[?([nac]\.)+\s\d{4}[\.?]?\]?|^\[\d{2}\]\d{2}\.)(\s[?[A-Z])'
+authorRegEx = r"\d\.*[\.\]]\s(Unknown\.|[A-Z]\.*,*\s*[A-Z]*\.|\[?(D')?(De\s)?([A-Z]?[a-z]+-)?([A-Z][a-z]+)(,\s)\[*([A-Z][a-z]+\s)*([A-Z][a-z]+)(,\s)*(of|de)*\s*([A-Z][a-z]+)*(\sof|de)*(\s[A-Z][a-z]+)*\(?\??\)?\.?\]?)|([A-Z]\[?[a-z]*\]?),\s([A-Z]\[?[a-z]*\]?)\."
+titleRegEx = r"([Ii]m[Pp]r[iy]nted|[Pp]rinted|Unique|, dated|[Ww]ritten|\[?\bin\b\]?|\b[Bb]ook\b|MS[S]?\.|\[?\b[cfv]ol\b|sign\.|\[?\bto\b|\b[pf]p?\b\.)"
 
 allusionsList = []
 
@@ -13,7 +14,6 @@ for txt in os.listdir(txtFolder):
   print(txt)
   txt_path = os.path.join(txtFolder, txt)
   label, ext = os.path.splitext(txt_path)
-  # print(label)
 
   if txt_path.endswith('.txt'):
     with open(txt_path, "r") as f:
@@ -22,15 +22,13 @@ for txt in os.listdir(txtFolder):
 
       volume = "Vol.2"
 
-      pageNb_head = re.search(r'^\d+\s|\d+\sCha', txtFile, flags=re.M)
-      pageNb = re.split(r'\s', pageNb_head.group())[0]
+      pageNb_head = re.search(r'^\d*\s|\s\d*$', txtFile, flags=re.M)
+      pageNb = re.split(r'\s', pageNb_head.group())
+      pageNb_clean = "".join(pageNb)
+      # print(pageNb_clean)
 
-      # print(pageNb)
-
-      date = re.search(dateRegEx,
-                      txtFile, flags=re.M).group()
-
-      # print(date[:-2])
+      date = re.search(dateRegEx, txtFile, flags=re.M).group(1)
+      # print(date)
 
       author = re.search(authorRegEx, txtFile, flags=re.M).group(1)
       # print(author)
@@ -38,17 +36,18 @@ for txt in os.listdir(txtFolder):
       text = re.split(authorRegEx, txtFile)
       # print(text[-1])
 
-      title = re.split(r'([Ii]m[Pp]r[iy]nted|[Pp]rinted|Unique|, dated|[Ww]ritten|\[?\bin\b\]?|\b[Bb]ook\b|MS[S]?\.|\[?\b[cf]ol\b|sign\.|\[?\bto\b|\bp[p]?\b\.)', text[-1], 1)
-      # print(title)
+      title = re.split(titleRegEx, text[-1], 1)
+      title_clean = re.sub(r'^\s', '', title[0])
+      # print(title[0])
 
       remainingText = ''.join(title[1:])
 
-      allusionsList.append([volume, pageNb, date[:-2], author, title[0], remainingText])
+      allusionsList.append([volume, pageNb_clean, date, author, title_clean, remainingText])
 
 print(allusionsList)
-'''fields = ['Volume', 'Page', 'Date', 'Author', 'Title','Text']
+fields = ['Volume', 'Page', 'Date', 'Author', 'Title','Quotation']
 
-with open('allusionsListTest5.csv', 'w', newline='') as c:
+with open('allusionsListTest8.csv', 'w', newline='') as c:
     writer = csv.writer(c)
     writer.writerow(fields)
-    writer.writerows(allusionsList)'''
+    writer.writerows(allusionsList)
