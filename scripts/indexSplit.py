d4463eff001e8843244996f1ddae2f3741ab0cae
leblance <elina.leblanc@live.fr>
2026-04-21 17:20:31 +0200

    Comment script and clean CSV

--- scripts/indexSplit.py
+++ scripts/indexSplit.py
@@ -5,39 +5,36 @@ file = "../biography/episodes/education.txt"
 folder = "../pages/vol3-pages/index/biography"
 infoList = []
 
-for subdir, dirs, files in os.walk(folder):
+for subdir, dirs, files in os.walk(folder):  # We parse the directories
     for file in files:
         if not file.endswith(".DS_Store"):
-            path = os.path.join(subdir, file)
+            path = os.path.join(subdir, file)  # Path to the file
             print(path)
 
             with open(path, "r") as f:
-                mainCat = path.split("/")[-3]
-                subCat = path.split("/")[-2]
-                subSubCat = path.split("/")[-1]
+                mainCat = path.split("/")[-3]  # Main category (First level of the index)
+                subCat = path.split("/")[-2]  # Subcategory (Second level of the index)
+                subSubCat = path.split("/")[-1]  # Sub-subcategory (Third level of the index)
 
                 textFile = f.read()
                 cleanedTextFile = textFile.replace("\n", " ")
 
-                entryLines = cleanedTextFile.split(";")
+                entryLines = cleanedTextFile.split(";")  # We split the text to get each entry
                 for entry in entryLines:
-                    entryInfo = entry.split(",")
+                    entryInfo = entry.split(",")  # We split the text to get portion of information
                     print(entryInfo)
 
+                    # From the split information, we get the author[0], the date[1], the volume and the vol/page[2]
                     authorName = entryInfo[0]
                     date = entryInfo[1]
                     pageInfo = entryInfo[2].split(" ")
 
+                    # All the information are stored in a list
                     infoList.append([mainCat, subCat, subSubCat[:-4], authorName, date, pageInfo[1][:-1].upper(), pageInfo[2]])
 
             # print(infoList[1])
 
-
-            '''if subSubCat != "":
-                csv_name = subSubCat[:-4] + '.csv'
-            else:
-                csv_name = subCat[:-4] + '.csv'''
-
+# The list is transformed into a CSV file corresponding to the main category
 fields = ['main_category', 'subcategory', 'sub_subcategory', 'author_name', 'date', 'volume', 'page']
 with open('biography.csv', 'w', newline='') as c:
     writer = csv.writer(c)
