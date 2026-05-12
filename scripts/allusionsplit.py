cf732305f6681353fdb82476b30351612c98499f
leblance <elina.leblanc@live.fr>
2026-05-12 11:00:39 +0200

    Add files for Supplement Batch 1

--- scripts/allusionsplit.py
+++ scripts/allusionsplit.py
@@ -1,8 +1,8 @@
 import re
 import os
 
-txtFolder = "../pages/vol2-pages/1876-1900"
-allusionFolder = "../allusions/vol2-allusions/1876-1900-allusions"
+txtFolder = "../pages/supplement"
+allusionFolder = "../allusions/supplement"
 
 for txt in sorted(os.listdir(txtFolder)):
   txt_path = os.path.join(txtFolder, txt)
@@ -31,7 +31,7 @@ for txt in sorted(os.listdir(txtFolder)):
       # Each entry is exported in a new .txt file
       allusionNb = 1
       for allusion in newText[1:]:
-        name = label[30:] + "_a" + str(allusionNb) + ".txt"
+        name = label[20:] + "_a" + str(allusionNb) + ".txt"
         allusionPath = os.path.join(allusionFolder, name)
 
         with open(allusionPath, 'w', newline='') as a:
